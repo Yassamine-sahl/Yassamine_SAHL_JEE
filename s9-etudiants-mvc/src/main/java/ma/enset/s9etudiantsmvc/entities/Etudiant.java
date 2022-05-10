689a9dd8b352b9a2483ae652d5e610108174cc56
Yassamine-sahl <85345273+Yassamine-sahl@users.noreply.github.com>
2022-05-11 00:52:29 +0200

    Gestion des etudiants : Chercher, Supprimer, Ajouter et Editer des etudiants

--- s9-etudiants-mvc/src/main/java/ma/enset/s9etudiantsmvc/entities/Etudiant.java
+++ s9-etudiants-mvc/src/main/java/ma/enset/s9etudiantsmvc/entities/Etudiant.java
@@ -3,8 +3,11 @@ package ma.enset.s9etudiantsmvc.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -15,11 +18,21 @@ public class Etudiant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 4, max = 40)
     private String nom;
+    @NotEmpty
     private String prenom;
+    @NotEmpty
     private String email;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateN;
+    @Enumerated
     private Genre genre;
     private Boolean regle;
 }
+
+
+
+
--- s9-etudiants-mvc/src/main/java/ma/enset/s9etudiantsmvc/web/EtudiantController.java
+++ s9-etudiants-mvc/src/main/java/ma/enset/s9etudiantsmvc/web/EtudiantController.java
@@ -7,14 +7,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-//l'injection en utilisant l'annotation AllArgsConstructor pour ajouter le constructeur avec parametre
+//l'injection en utilisant l'annotation AllArgsConstructor
 @AllArgsConstructor
 public class EtudiantController {
 
@@ -26,9 +29,10 @@ public class EtudiantController {
                             @RequestParam(name = "size", defaultValue = "5") int size,
                             @RequestParam(name = "keyword", defaultValue = "") String keyword){
 
-        //Afficher la liste des etudiants
-        //List<Etudiant> etudiants = etudiantRepository.findAll();
-        // model.addAttribute("listEtudiants", etudiants);
+        /* Afficher la liste des etudiants
+         List<Etudiant> etudiants = etudiantRepository.findAll();
+         model.addAttribute("listEtudiants", etudiants);
+         */
 
         //Afficher 5 etudiants par Page (La pagination)
         Page<Etudiant> pageEtudiants = etudiantRepository.findByNomContains(keyword, PageRequest.of(page, size));
@@ -66,4 +70,52 @@ public class EtudiantController {
     public List<Etudiant> etudiantList() {
         return etudiantRepository.findAll();
     }
+
+    @GetMapping("/formEtudiants")
+    public String formEtudiants(Model model){
+        model.addAttribute("etudiant",new Etudiant());
+        return "formEtudiants";
+    }
+
+    @PostMapping(path = "/save")
+    public String save(Model model, @Valid Etudiant etudiant, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "") String keyword){
+        if (bindingResult.hasErrors()) return "formEtudiants";
+        etudiantRepository.save(etudiant);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editEtudiant")
+    public String editEtudiant(Model model, Long id, String keyword, int page){
+        Etudiant etudiant=etudiantRepository.findById(id).orElse(null);
+        if (etudiant==null) throw new RuntimeException("Etudiant introuvable");
+        model.addAttribute("etudiant",etudiant);
+        model.addAttribute("etudiant",etudiant);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editEtudiant";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
